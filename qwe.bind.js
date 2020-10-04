97d9008b542bd99453d32f68496fc30b29810da1
Muhaza <30515291+muhaza@users.noreply.github.com>
2020-10-04 19:49:14 +0800

    qwe

---
+++ qwe.bind.js
@@ -0,0 +1,5 @@
+var qwe = document.querySelector.bind(document);
+var qweAll = document.querySelectorAll.bind(document);
+var qweId = document.getElementById.bind(document);
+var qwelass = document.getElementsByClassName.bind(document);
+var qweTag = document.getElementsByTagName.bind(document);
